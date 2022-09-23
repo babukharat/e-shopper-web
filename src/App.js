dd0c811a85ba7b761cbfde7f131221d09326c180
babukharat <kharatbabu55@gmail.com>
2022-09-23 11:43:31 +0530

    first

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import Layout from "./inventry-module/Layout";
 
 function App() {
-  return <Layout />;
+  return (
+    <>
+      <Layout />;
+    </>
+  );
 }
 
 export default App;
